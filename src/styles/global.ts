15b5b84f67bb2b2f6c95cc3c8d560d93ecb90d55
Cauê Ferreira <kau3hc@hotmail.com>
2021-08-19 19:25:38 -0300

    component: summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,6 +6,7 @@ export const GlobalStyle = createGlobalStyle`
     :root{
         --background: #f0f2f5;
         --red:#e52e4d;
+        --green:#33cc95;
         --blue: #5429cc;
         --blue-light: #6933ff;
         --text-title:#363f5f;
@@ -33,7 +34,13 @@ export const GlobalStyle = createGlobalStyle`
          background: var(--background);
          -webkit-font-smoothing: antialiased;
          }
-
+    body, input, textarea,button{
+        font-family: "Poppins", sans-serif;
+        font-weight:400;
+    }
+    h1,h2,h3,h4,h5,h6,strong{
+        font-weight: 600;
+    }     
     button{
         cursor: pointer;
     }     
