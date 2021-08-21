41ff2fa104dba15a3319e2c606899729c45da131
Cauê Ferreira <kau3hc@hotmail.com>
2021-08-21 17:58:49 -0300

    modal de criação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionOpen] = useState(false);
+
+  function handleOpenTransactionModal() {
+    setIsNewTransactionOpen(true);
+  }
+  function handleCloseTransactionModal() {
+    setIsNewTransactionOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      >
+        <h2>cadastrar transacao</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
