857edc554c84bef9ff760ffc13ecbd6baea04364
Cauê Ferreira <kau3hc@hotmail.com>
2021-08-23 19:16:17 -0300

    estilizando modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -64,10 +64,24 @@ export const GlobalStyle = createGlobalStyle`
 
     .react-modal-content{
         width: 100%;
-        max-width:576px;
+        max-width: 576px;
         background: var(--background);
-        padding:3rem;
+        padding: 3rem;
         position: relative;
-        border-radius: 0.24rem;
+        border-radius: 0.24rem; 
+    }
+
+    .react-modal-close{
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+
+        transition: filter 0.2s;
+
+        &:hover{
+            filter: brightness(0.8);
+        }
     }
 `
