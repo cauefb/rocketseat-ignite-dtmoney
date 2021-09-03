2b58565c56f00d2b1e9c980a8ffbe69a88fd680f
Cauê Ferreira <kau3hc@hotmail.com>
2021-09-03 15:51:23 -0300

    Finalizando inserção

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,6 @@
 import Modal from "react-modal";
 import { FormEvent, useState, useContext } from "react";
 import { TransactionsContext } from "../../TransactionsContext";
-import { api } from "../../services/api";
 
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
@@ -25,14 +24,21 @@ export function NewTransactionModal({
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type,
     });
+
+    setTitle("");
+    setAmount(0);
+    setCategory("");
+    setType("deposit");
+
+    onRequestClose();
   }
 
   return (
